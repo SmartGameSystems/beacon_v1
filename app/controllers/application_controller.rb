43d2f42973a3e4ecbf0120316f11ef099057687d
TurnerMooreDavis <turnermdavis@gmail.com>
2017-11-10 09:41:35 -0500

    initial view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  
+  def index
+    Rails.logger.info "in index"
+    render "layouts/application.html.erb"
+  end
+  
 end
