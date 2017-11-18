40decf15f3c9dcf58c8c1540fc973e9cb96b20fd
Gabriel Estes <gabriel.estes@mckesson.com>
2017-11-18 13:55:30 -0500

    modify profile attributes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,9 @@
 class Profile < ApplicationRecord
+  belongs_to :profilable, polymorphic: true
+  # Set each type of user to profilable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
--- db/migrate/20171111220456_devise_create_profiles.rb
+++ db/migrate/20171111220456_devise_create_profiles.rb
@@ -4,6 +4,10 @@ class DeviseCreateProfiles < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :username
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
 
       ## Recoverable
       t.string   :reset_password_token
@@ -16,27 +20,19 @@ class DeviseCreateProfiles < ActiveRecord::Migration[5.1]
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
 
     add_index :profiles, :email,                unique: true
     add_index :profiles, :reset_password_token, unique: true
-    # add_index :profiles, :confirmation_token,   unique: true
-    # add_index :profiles, :unlock_token,         unique: true
+    add_index :profiles, :confirmation_token,   unique: true
+    add_index :profiles, :unlock_token,         unique: true
   end
 end
