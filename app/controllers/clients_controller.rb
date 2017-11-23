c02bea532ad51af2fa37f8c272e1be744668fc93
Gabriel Estes <gabriel.estes@mckesson.com>
2017-11-23 12:03:47 -0500

    add clients controller and basic actions

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,24 @@
+class ClientsController < ApplicationController
+
+  # GET /
+  def index
+  end
+
+  # GET /client
+  def show
+  end
+
+  # GET /client/edit
+  def edit
+    # edit
+  end
+
+  # PUT /client
+  def update
+  end
+
+  # DELETE /
+  def destroy
+    # The "/" route will redirect to sign_in/sign_up page with a "successfully deleted message"
+  end
+end
