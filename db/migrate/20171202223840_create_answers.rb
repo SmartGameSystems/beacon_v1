a51daa62661bb7c4af6c179a5edf4ce469e226b0
Gabriel Estes <gabriel.estes@mckesson.com>
2017-12-02 17:55:02 -0500

    add indexes to answers migration

--- db/migrate/20171202223840_create_answers.rb
+++ db/migrate/20171202223840_create_answers.rb
@@ -2,6 +2,9 @@ class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
       t.text :body
+      t.belongs_to :question, index: true
+      t.belongs_to :contact, index: true
+      t.belongs_to :client, index: true
 
       t.timestamps
     end
